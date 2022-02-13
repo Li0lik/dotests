8cda1daee751c324af4c580c6ec7a3373b98759f
Alexey.Tsygankov <alexey.tsygankov@myqops.com>
2022-02-13 12:32:48 +0300

    added just a test

---
+++ runner/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+	"time"
+)
+
+func Run() {
+	cmd := exec.Command("hello.exe")
+
+	err := cmd.Start()
+	if err != nil {
+		log.Println(err.Error())
+	}
+	err = cmd.Wait()
+	cmd = nil
+}
+func main() {
+	for {
+		Run()
+		time.Sleep(5 * time.Second)
+		//runtime.GC()
+	}
+}
